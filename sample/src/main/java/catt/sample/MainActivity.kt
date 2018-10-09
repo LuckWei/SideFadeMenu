a5bd89a7e29876e31cf5319c21e2e587aa8b2e46
zhiwei <zhiwei@htech.vip>
2018-10-09 14:36:07 +0800

    r2

--- sample/src/main/java/catt/sample/MainActivity.kt
+++ sample/src/main/java/catt/sample/MainActivity.kt
@@ -3,6 +3,7 @@ package catt.sample
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log.e
+import android.widget.Toast
 import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.include_content_layout.*
 import kotlinx.android.synthetic.main.include_menu_layout.*
@@ -14,10 +15,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         menuBtn.setOnClickListener {
             e(_TAG, "##################    onClick: menuBtn")
-
+            Toast.makeText(applicationContext, "onClick: menu Btn", Toast.LENGTH_SHORT).show()
         }
         contentBtn.setOnClickListener {
             e(_TAG, "@@@@@@@@@@@@@@@@@@    onClick: contentBtn")
+            Toast.makeText(applicationContext, "onClick: content Btn", Toast.LENGTH_SHORT).show()
         }
     }
 
